c7595b0c9d726bf33882f52891155163c2efb4a9
Clayton Jensen <clayton@yourmommas.house>
2025-02-14 17:02:11 -0600

    fix editor issues

--- apps/mobile/app/(auth)/login.tsx
+++ apps/mobile/app/(auth)/login.tsx
@@ -1,63 +1,68 @@
-import React from 'react'
+import React from "react";
 
-import { makeRedirectUri } from 'expo-auth-session'
-import { Provider } from '@supabase/supabase-js'
-import { supabase } from '@/lib/supabase'
-import * as WebBrowser from "expo-web-browser"
-import * as QueryParams from 'expo-auth-session/build/QueryParams'
-import { View } from 'react-native'
-import { Stack } from 'expo-router'
-import { ProviderButtons } from '@/components/provider-buttons'
+import { makeRedirectUri } from "expo-auth-session";
+import { supabase } from "@/lib/supabase";
+import * as WebBrowser from "expo-web-browser";
+import type { Provider } from "@the-leftovers/supabase";
+import * as QueryParams from "expo-auth-session/build/QueryParams";
+import { View } from "react-native";
+import { Stack } from "expo-router";
+import { ProviderButtons } from "@/components/provider-buttons";
 
 export default function LoginScreen() {
-    const redirectTo = makeRedirectUri()
+  const redirectTo = makeRedirectUri();
 
-    async function signInWithProvider(provider: Provider) {
-        const { data, error } = await supabase.auth.signInWithOAuth({
-            provider,
-            options: {
-                redirectTo,
-                skipBrowserRedirect: true
-            }
-        })
+  async function signInWithProvider(provider: Provider) {
+    const { data, error } = await supabase.auth.signInWithOAuth({
+      provider,
+      options: {
+        redirectTo,
+        skipBrowserRedirect: true,
+      },
+    });
 
-        if (error) {
-            console.log("🔥 ~ signInWithProvider:", error)
-            throw error
-        }
-
-        const res = await WebBrowser.openAuthSessionAsync(data?.url ?? '', redirectTo)
-
-        if (res.type === 'success') {
-            await createSessionFromUrl(res.url)
-        }
+    if (error) {
+      console.log("🔥 ~ signInWithProvider:", error);
+      throw error;
     }
 
-    async function createSessionFromUrl(url: string) {
-        const { params, errorCode } = QueryParams.getQueryParams(url)
+    const res = await WebBrowser.openAuthSessionAsync(
+      data?.url ?? "",
+      redirectTo
+    );
 
-        if (errorCode) throw new Error(errorCode)
-        const { access_token, refresh_token } = params
+    if (res.type === "success") {
+      await createSessionFromUrl(res.url);
+    }
+  }
 
-        if (!access_token) return
+  async function createSessionFromUrl(url: string) {
+    const { params, errorCode } = QueryParams.getQueryParams(url);
 
-        const { data, error } = await supabase.auth.setSession({
-            access_token,
-            refresh_token
-        })
+    if (errorCode) throw new Error(errorCode);
+    const { access_token, refresh_token } = params;
 
-        if (error) {
-            console.log("🔥 ~ createSessionFromUrl:", error)
-            throw error
-        }
+    if (!access_token) return;
 
-        return data.session
+    const { data, error } = await supabase.auth.setSession({
+      access_token,
+      refresh_token,
+    });
+
+    if (error) {
+      console.log("🔥 ~ createSessionFromUrl:", error);
+      throw error;
     }
 
-    return (
-        <View>
-            <Stack.Screen options={{ headerShown: true, title: "Log in to ATU Bulletin" }} />
-            <ProviderButtons handler={signInWithProvider} />
-        </View>
-    )
+    return data.session;
+  }
+
+  return (
+    <View>
+      <Stack.Screen
+        options={{ headerShown: true, title: "Log in to ATU Bulletin" }}
+      />
+      <ProviderButtons handler={signInWithProvider} />
+    </View>
+  );
 }
--- apps/mobile/app/(tabs)/_layout.tsx
+++ apps/mobile/app/(tabs)/_layout.tsx
@@ -1,83 +1,101 @@
 import React, { useEffect, useState } from "react";
 import { Tabs } from "expo-router";
 
-import { CalendarIcon, HomeIcon, UserIcon, UsersIcon } from "lucide-react-native"
+import {
+  CalendarIcon,
+  HomeIcon,
+  UserIcon,
+  UsersIcon,
+} from "lucide-react-native";
 import { supabase } from "@/lib/supabase";
 import { Image } from "react-native";
 
 interface Screen {
-    route: string
-    title: string
-    icon: ({ size, color }: { size: number; color: string }) => JSX.Element
+  route: string;
+  title: string;
+  icon: ({ size, color }: { size: number; color: string }) => JSX.Element;
 }
 
 const screens: Screen[] = [
-    {
-        route: "home",
-        title: "Home",
-        icon: ({ size, color }) => <HomeIcon size={size} color={color} />
-    },
-    {
-        route: "events",
-        title: "Events",
-        icon: ({ size, color }) => <CalendarIcon size={size} color={color} />
-    },
-    {
-        route: "organizations/index",
-        title: "Organizations",
-        icon: ({ size, color }) => <UsersIcon size={size} color={color} />
-    },
-    {
-        route: "profile",
-        title: "Profile",
-        icon: ({ size, color }) => <UserIcon size={size} color={color} />
-    }
-]
+  {
+    route: "home",
+    title: "Home",
+    icon: ({ size, color }) => <HomeIcon size={size} color={color} />,
+  },
+  {
+    route: "events",
+    title: "Events",
+    icon: ({ size, color }) => <CalendarIcon size={size} color={color} />,
+  },
+  {
+    route: "organizations/index",
+    title: "Organizations",
+    icon: ({ size, color }) => <UsersIcon size={size} color={color} />,
+  },
+  {
+    route: "profile",
+    title: "Profile",
+    icon: ({ size, color }) => <UserIcon size={size} color={color} />,
+  },
+];
 
 export default function TabsLayout() {
-    const [profileImageUrl, setProfileImageUrl] = useState<string | null>(null)
-    
-    useEffect(() => {
-        async function fetchProfileImage() {
-            const { data: { user }, error } = await supabase.auth.getUser()
+  const [profileImageUrl, setProfileImageUrl] = useState<string | null>(null);
 
-            if (error) {
-                console.log("🔥 ~ fetchProfileImage:", error)
-                return;
-            }
+  useEffect(() => {
+    async function fetchProfileImage() {
+      const {
+        data: { user },
+        error,
+      } = await supabase.auth.getUser();
 
-            if (user) {
-                setProfileImageUrl(user.user_metadata.avatar_url)
-            }
-        }
+      if (error) {
+        console.log("🔥 ~ fetchProfileImage:", error);
+        return;
+      }
 
-        fetchProfileImage()
-    }, [])
+      if (user) {
+        setProfileImageUrl(user.user_metadata.avatar_url);
+      }
+    }
 
-    return (
-        <Tabs screenOptions={{ headerShown: false, tabBarActiveTintColor: '#ffcd00', tabBarInactiveTintColor: '#fff', tabBarStyle: { backgroundColor: '#4b7f52' }, tabBarLabelStyle: { fontWeight: 'bold' } }}>
-            {screens.map((screen) => (
-                <Tabs.Screen 
-                    key={screen.route}
-                    name={screen.route}
-                    options={{
-                        title: screen.title,
-                        tabBarIcon: ({ color }) => {
-                            if (screen.route === "profile" && profileImageUrl) {
-                                return (
-                                    <Image source={{ uri: profileImageUrl }} style={{ width: 24, height: 24, borderRadius: 12 }} />
-                                )
-                            }
+    fetchProfileImage();
+  }, []);
 
-                            return <screen.icon size={24} color={color} />
-                        }
-                    }}
-                />
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: false,
+        tabBarActiveTintColor: "#ffcd00",
+        tabBarInactiveTintColor: "#fff",
+        tabBarStyle: { backgroundColor: "#4b7f52" },
+        tabBarLabelStyle: { fontWeight: "bold" },
+      }}
+    >
+      {screens.map((screen) => (
+        <Tabs.Screen
+          key={screen.route}
+          name={screen.route}
+          options={{
+            title: screen.title,
+            tabBarIcon: ({ color }) => {
+              if (screen.route === "profile" && profileImageUrl) {
+                return (
+                  <Image
+                    source={{ uri: profileImageUrl }}
+                    style={{ width: 24, height: 24, borderRadius: 12 }}
+                  />
+                );
+              }
 
-            ))}
+              return <screen.icon size={24} color={color} />;
+            },
+          }}
+        />
+      ))}
 
-            {/* Trick to exclude routes from the tab bar */}
-            <Tabs.Screen name="organizations/[id]" options={{ href: null }}/>
-        </Tabs>
-    )
+      {/* Trick to exclude routes from the tab bar */}
+      <Tabs.Screen name="organizations/[id]" options={{ href: null }} />
+    </Tabs>
+  );
 }
