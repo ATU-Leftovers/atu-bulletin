3ca965a5f8a5368c8a463326b2820c0404824a76
jgardp <jgardunopena@atu.edu>
2025-02-23 22:47:27 -0600

    Updated styles for Your Events page

--- apps/mobile/app/(profile)/yourEvents.tsx
+++ apps/mobile/app/(profile)/yourEvents.tsx
@@ -1,8 +1,10 @@
 import { SafeAreaView } from "react-native-safe-area-context"
-import { FlatList, ScrollView, Text, View, Image, Pressable } from "react-native";
+import { FlatList, ScrollView, Text, View, Image, Pressable, StyleSheet } from "react-native";
 import { useEffect, useState } from "react";
 import MaterialCommunityIcons from "@expo/vector-icons/MaterialCommunityIcons";
 import { Link } from 'expo-router';
+import { ThemeColors } from '@/constants/Colors';
+import ListingComponent from '@/components/listingComponent';
 
 export default function YourEvents() {
     //will need to assign proper data usestates for each
@@ -18,11 +20,11 @@ export default function YourEvents() {
 
 
     return (
-        <SafeAreaView className="flex-1">
-            <ScrollView className="gap-5 py-4">
-                <View className='flex pt-2 pb-4 bg-atu-gold-vl'>
-                    <View className="flex-row justify-between pl-3 pr-2">
-                        <Text className='text-lg font-bold text-white '>Your Hosted Events</Text>
+        <SafeAreaView style={styles.container}>
+            <ScrollView style={styles.scroll} showsVerticalScrollIndicator={false}>
+                <View style={styles.eventType}>
+                    <View style={styles.hostedEvent} >
+                        <Text style={styles.titleText}>Your Hosted Events</Text>
                         <Link href="/addEvent" asChild>
                             <Pressable>
                                 <MaterialCommunityIcons name="plus-circle" size={28} color="white" />
@@ -32,18 +34,18 @@ export default function YourEvents() {
 
                     {/* No Hosted Events */}
                     {userRegisteredEvents.length == 0 && (
-                        <View className='items-center self-center justify-center object-center w-2/3 p-6 mt-2 mb-4 bg-white rounded-md'>
+                        <View style={styles.noEvents}>
                             <Text >Not Hosting Any Events</Text>
                             <Pressable>
-                                <View className="px-3 py-1 mt-2 rounded-full bg-atu-gold-vd">
-                                    <Text className="font-bold text-white">Host Events</Text>
+                                <View style={styles.button}>
+                                    <Text style={styles.textWhite}>Host Events</Text>
                                 </View>
                             </Pressable>
                         </View>)}
 
                     {userRegisteredEvents.length != 0 && (
                         <FlatList
-                            className='py-1'
+                            style={styles.flatListStyle}
                             data={userRegisteredEvents}
                             horizontal={true}
                             showsHorizontalScrollIndicator={false}
@@ -51,21 +53,21 @@ export default function YourEvents() {
                                 //Last Item Redirect
                                 if (index == currentEventData.length - 1) {
                                     return (
-                                        <View className='items-center justify-center mx-4 bg-gray-600 h-44 w-28'>
+                                        <View style={styles.flatListItemLastContainer}>
                                             <MaterialCommunityIcons name="plus-thick" size={24} color="white" />
-                                            <Text className='font-bold text-white'>View More</Text>
+                                            <Text style={styles.textWhite}>View More</Text>
                                         </View>
                                     )
                                 }
                                 //Render Regular Items
                                 else {
                                     return (
-                                        <View className='ml-4 bg-gray-600 h-44 w-28'>
-                                            <Image className="bg-gray-400 h-28 aspect-square" source={require('../../assets/images/Pickle.png')} />
-                                            <View className='h-full gap-1 p-1'>
-                                                <Text numberOfLines={1} ellipsizeMode='tail' className='text-xs text-gray-200'>Autddddddddddddddddddddddddddddddddddddddddadsahor</Text>
-                                                <Text numberOfLines={1} ellipsizeMode='tail' className='text-xs font-bold text-white'>Titdwadwwwwwwwwwwwwwwwwwwwwwwwwwwwwwle</Text>
-                                                <Text numberOfLines={1} ellipsizeMode='tail' className='text-xs text-gray-200'>Publish:Date, Timewwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww</Text>
+                                        <View style={styles.flatListItemContainer}>
+                                            <Image style={styles.flatListItemImage} source={require('../../assets/images/Pickle.png')} />
+                                            <View style={styles.flatListItemTextContainer}>
+                                                <Text numberOfLines={1} ellipsizeMode='tail' style={styles.smallGreyText}>Autddddddddddddddddddddddddddddddddddddddddadsahor</Text>
+                                                <Text numberOfLines={1} ellipsizeMode='tail' style={styles.smallWhiteText}>Titdwadwwwwwwwwwwwwwwwwwwwwwwwwwwwwwle</Text>
+                                                <Text numberOfLines={1} ellipsizeMode='tail' style={styles.smallGreyText}>Publish:Date, Timewwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww</Text>
                                             </View>
                                         </View>
                                     )
@@ -75,23 +77,23 @@ export default function YourEvents() {
                     )}
                 </View>
 
-                <View className='flex pt-2 pb-4 bg-atu-gold-vl'>
-                    <Text className='px-3 text-lg font-bold text-white'>Registered Events</Text>
+                <View style={styles.eventType}>
+                    <Text style={styles.titleText}>Registered Events</Text>
 
                     {/* No Registered Events */}
                     {userRegisteredEvents.length == 0 && (
-                        <View className='items-center self-center justify-center object-center w-2/3 p-6 mt-2 mb-4 bg-white rounded-md'>
+                        <View style={styles.noEvents}>
                             <Text >No Registered Events</Text>
                             <Pressable>
-                                <View className="px-3 py-1 mt-2 rounded-full bg-atu-gold-vd">
-                                    <Text className="font-bold text-white">Find Events</Text>
+                                <View style={styles.button}>
+                                    <Text style={styles.textWhite}>Find Events</Text>
                                 </View>
                             </Pressable>
                         </View>)}
 
                     {userRegisteredEvents.length != 0 && (
                         <FlatList
-                            className='py-1'
+                            style={styles.flatListStyle}
                             data={userRegisteredEvents}
                             horizontal={true}
                             showsHorizontalScrollIndicator={false}
@@ -99,21 +101,21 @@ export default function YourEvents() {
                                 //Last Item Redirect
                                 if (index == currentEventData.length - 1) {
                                     return (
-                                        <View className='items-center justify-center mx-4 bg-gray-600 h-44 w-28'>
+                                        <View style={styles.flatListItemLastContainer}>
                                             <MaterialCommunityIcons name="plus-thick" size={24} color="white" />
-                                            <Text className='font-bold text-white'>View More</Text>
+                                            <Text style={styles.textWhite}>View More</Text>
                                         </View>
                                     )
                                 }
                                 //Render Regular Items
                                 else {
                                     return (
-                                        <View className='ml-4 bg-gray-600 h-44 w-28'>
-                                            <Image className="bg-gray-400 h-28 aspect-square" source={require('../../assets/images/Pickle.png')} />
-                                            <View className='h-full gap-1 p-1'>
-                                                <Text numberOfLines={1} ellipsizeMode='tail' className='text-xs text-gray-200'>Autddddddddddddddddddddddddddddddddddddddddadsahor</Text>
-                                                <Text numberOfLines={1} ellipsizeMode='tail' className='text-xs font-bold text-white'>Titdwadwwwwwwwwwwwwwwwwwwwwwwwwwwwwwle</Text>
-                                                <Text numberOfLines={1} ellipsizeMode='tail' className='text-xs text-gray-200'>Publish:Date, Timewwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww</Text>
+                                        <View style={styles.flatListItemContainer}>
+                                            <Image style={styles.flatListItemImage} source={require('../../assets/images/Pickle.png')} />
+                                            <View style={styles.flatListItemTextContainer}>
+                                                <Text numberOfLines={1} ellipsizeMode='tail' style={styles.smallGreyText}>Autddddddddddddddddddddddddddddddddddddddddadsahor</Text>
+                                                <Text numberOfLines={1} ellipsizeMode='tail' style={styles.smallWhiteText}>Titdwadwwwwwwwwwwwwwwwwwwwwwwwwwwwwwle</Text>
+                                                <Text numberOfLines={1} ellipsizeMode='tail' style={styles.smallGreyText}>Publish:Date, Timewwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww</Text>
                                             </View>
                                         </View>
                                     )
@@ -121,20 +123,22 @@ export default function YourEvents() {
                             }}
                         />
                     )}
+                    
+
                 </View>
 
-                <View className='flex pt-2 pb-4 bg-atu-gold-vl'>
-                    <Text className='px-3 text-lg font-bold text-white'>Event History</Text>
+                <View style={styles.eventType}>
+                    <Text style={styles.titleText}>Event History</Text>
 
                     {/* Event History */}
                     {userRegisteredEvents.length == 0 && (
-                        <View className='items-center self-center justify-center object-center w-2/3 p-10 mt-2 mb-4 bg-white rounded-md'>
+                        <View style={styles.noEvents}>
                             <Text >No Event History</Text>
                         </View>)}
 
-                    {userRegisteredEvents.length != 0 && (
+                        {userRegisteredEvents.length != 0 && (
                         <FlatList
-                            className='py-1'
+                            style={styles.flatListStyle}
                             data={userRegisteredEvents}
                             horizontal={true}
                             showsHorizontalScrollIndicator={false}
@@ -142,21 +146,21 @@ export default function YourEvents() {
                                 //Last Item Redirect
                                 if (index == currentEventData.length - 1) {
                                     return (
-                                        <View className='items-center justify-center mx-4 bg-gray-600 h-44 w-28'>
+                                        <View style={styles.flatListItemLastContainer}>
                                             <MaterialCommunityIcons name="plus-thick" size={24} color="white" />
-                                            <Text className='font-bold text-white'>View More</Text>
+                                            <Text style={styles.textWhite}>View More</Text>
                                         </View>
                                     )
                                 }
                                 //Render Regular Items
                                 else {
                                     return (
-                                        <View className='ml-4 bg-gray-600 h-44 w-28'>
-                                            <Image className="bg-gray-400 h-28 aspect-square" source={require('../../assets/images/Pickle.png')} />
-                                            <View className='h-full gap-1 p-1'>
-                                                <Text numberOfLines={1} ellipsizeMode='tail' className='text-xs text-gray-200'>Autddddddddddddddddddddddddddddddddddddddddadsahor</Text>
-                                                <Text numberOfLines={1} ellipsizeMode='tail' className='text-xs font-bold text-white'>Titdwadwwwwwwwwwwwwwwwwwwwwwwwwwwwwwle</Text>
-                                                <Text numberOfLines={1} ellipsizeMode='tail' className='text-xs text-gray-200'>Publish:Date, Timewwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww</Text>
+                                        <View style={styles.flatListItemContainer}>
+                                            <Image style={styles.flatListItemImage} source={require('../../assets/images/Pickle.png')} />
+                                            <View style={styles.flatListItemTextContainer}>
+                                                <Text numberOfLines={1} ellipsizeMode='tail' style={styles.smallGreyText}>Autddddddddddddddddddddddddddddddddddddddddadsahor</Text>
+                                                <Text numberOfLines={1} ellipsizeMode='tail' style={styles.smallWhiteText}>Titdwadwwwwwwwwwwwwwwwwwwwwwwwwwwwwwle</Text>
+                                                <Text numberOfLines={1} ellipsizeMode='tail' style={styles.smallGreyText}>Publish:Date, Timewwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwwww</Text>
                                             </View>
                                         </View>
                                     )
@@ -164,9 +168,108 @@ export default function YourEvents() {
                             }}
                         />
                     )}
+
                 </View>
 
             </ScrollView>
         </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+    },
+    scroll: {
+        gap: 17,
+        paddingTop: 11,
+        paddingBottom: 21,
+    },
+    eventType: {
+        display: 'flex',
+        paddingTop: 10,
+        paddingBottom: 10,
+        backgroundColor: ThemeColors['atu-gold-vl'],
+        marginBottom: 20,
+    },
+    hostedEvent: {
+       flexDirection: 'row',
+       justifyContent: 'space-between',
+       paddingLeft: 16,
+       paddingRight: 11,
+    },
+    titleText: {
+       fontSize: 20,
+       lineHeight: 27,
+       fontWeight: 'bold',
+       color: 'white',
+       paddingLeft: 10,
+     },
+    noEvents: {
+        alignItems: 'center',
+        alignSelf: 'center',
+        justifyContent: 'center',
+        width: '66%',
+        padding: 22,
+        marginTop: 11,
+        marginBottom: 21,
+        backgroundColor: 'white',
+        borderRadius: 8,
+
+        // object-center
+        // w-2/3
+        // rounded-md
+      },
+      button: {
+        paddingLeft: 16,
+        paddingRight: 16,
+        paddingTop: 5,
+        paddingBottom: 5,
+        marginTop: 10,
+        borderRadius: 10,
+        backgroundColor: ThemeColors['atu-gold-vd']
+
+        // rounded-full
+      },
+      textWhite: {
+        fontWeight: 'bold',
+        color: 'white',
+      },
+      flatListStyle: {
+        paddingVertical: 4
+      },
+      flatListItemLastContainer: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginHorizontal: 16,
+        backgroundColor: ThemeColors['gray'],
+        height: 182,
+        width: 112,
+      },
+      flatListItemContainer: {
+        marginLeft: 16,
+        backgroundColor: ThemeColors['gray'],
+        height: 182,
+        width: 112,
+      },
+      flatListItemImage: {
+        backgroundColor: ThemeColors['gray'],
+        height: 112,
+        aspectRatio: 1 / 1,
+      },
+      flatListItemTextContainer: {
+        height: '100%',
+        gap: 4,
+        padding: 4,
+      },
+      smallGreyText: {
+        fontSize: 12,
+        color: '#e5e7eb',
+      },
+      smallWhiteText:{
+        fontSize: 12,
+        fontWeight: 'bold',
+        color: 'white',
+      }
+
+})
